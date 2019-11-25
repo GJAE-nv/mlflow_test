3825194ba5f9f604b102c75191a8afa907a06810
vrankenjulie <50368245+vrankenjulie@users.noreply.github.com>
2019-11-25 17:31:13 +0100

    Update Houses_Model.py

--- ml_flow_azure_vm/Houses_Model.py
+++ ml_flow_azure_vm/Houses_Model.py
@@ -125,8 +125,8 @@ if __name__ == "__main__":
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
 
-        mlflow.log_metric("alpha", alpha)
-        mlflow.log_metric("l1_ratio", l1_ratio)
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
